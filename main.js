932d2c844b716a2a7b75d12ce0e183f2d7e30e99
Diego Lopes <d.lopes.karagua@gmail.com>
2019-10-15 14:49:51 -0300

    Plotter

--- main.js
+++ main.js
@@ -1,36 +1,76 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     let data = [];
+    let currentData;
 
-    $('#btn').click(function(){
+    $('#btn').click(function () {
 
         $.post({
-            url : 'http://169.254.125.171',
-            beforeSend : function(){
+            url: 'http://169.254.125.171',
+            beforeSend: function () {
                 $('#response').html('Tentando fazer requisição...')
             }
-        }).done(function(response){
+        }).done(function (response) {
             $('#response').html(response.number)
             console.log(response);
         })
- 
+
     });
 
 
-    function requestData(){
+    function requestData() {
         $.post({
-            url : 'http://169.254.125.171',
-            beforeSend : function(){
+            url: 'http://169.254.45.198',
+            beforeSend: function () {
                 $('#response').html('Tentando fazer requisição...')
             }
-        }).done(function(response){
+        }).done(function (response) {
             $('#response').html(response.number)
             //console.log(response);
             data.push(response.number);
-            console.log(data);
+            currentData = response.number
+            console.log(currentData);
         })
     }
 
-    setInterval(requestData,2000);
+    setInterval(requestData, 500);
+
+
+
+    // Gráficos com plotty
+    var arrayLength = 20
+    var newArray = []
+
+    for (var i = 0; i < arrayLength; i++) {
+        var y = data[i]
+        newArray[i] = y
+    }
+
+    Plotly.plot('graph', [{
+        y: newArray,
+        mode: 'lines',
+        line: {
+            color: 'red'
+        }
+    }]);
+
+    var cnt = 0;
+
+    var interval = setInterval(function () {
+
+        var y = currentData ;
+        newArray = newArray.concat(y)
+        newArray.splice(0, 1)
+
+        var data_update = {
+            y: [newArray]
+        };
+
+        Plotly.update('graph', data_update)
+
+        if (cnt === 100) clearInterval(interval);
+    }, 500);
+
+
 
 })
--- server/server.ino
+++ server/server.ino
@@ -1,38 +1,17 @@
-/*
-  Web Server
-
- A simple web server that shows the value of the analog input pins.
- using an Arduino Wiznet Ethernet shield.
-
- Circuit:
- * Ethernet shield attached to pins 10, 11, 12, 13
- * Analog inputs attached to pins A0 through A5 (optional)
-
- created 18 Dec 2009
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe
- modified 02 Sept 2015
- by Arturo Guadalupi
- 
- */
-
 #include <SPI.h>
 #include <Ethernet.h>
 
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
+
+//Configurar a conexão antes de iniciar:
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
-IPAddress ip(169, 254, 125, 171);
-
-IPAddress subnet(255, 255, 0, 0); //Define a máscara de rede
-
-// (port 80 is default for HTTP):
+IPAddress ip(169, 254, 45, 198);
+IPAddress subnet(255, 255, 0, 0); 
 EthernetServer server(80);
 
 long randNumber;
+
 
 void setup() {
 
@@ -48,18 +27,18 @@ void setup() {
 
   randomSeed(analogRead(0));
 
-  // Check for Ethernet hardware present
+  // Verificar se o Ethernet está OK
   if (Ethernet.hardwareStatus() == EthernetNoHardware) {
     Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
     while (true) {
-      delay(1); // do nothing, no point running without Ethernet hardware
+      delay(1); // impede de rodar sem o Ethernet Shield
     }
   }
   if (Ethernet.linkStatus() == LinkOFF) {
     Serial.println("Ethernet cable is not connected.");
   }
 
-  // start the server
+  // Iniciar o  servidor!
   server.begin();
   Serial.print("server is at ");
   Serial.println(Ethernet.localIP());
@@ -70,25 +49,26 @@ void loop() {
 
   randNumber = random(300);
   
-  // listen for incoming clients
+  // atendendo os 'clientes'
   EthernetClient client = server.available();
   if (client) {
     Serial.println("new client");
-    // an http request ends with a blank line
+    // Termina a requisição HTTP com uma linha em branco no final
     bool currentLineIsBlank = true;
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
         Serial.write(c);
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the http request has ended,
-        // so you can send a reply
+
+
+
         if (c == '\n' && currentLineIsBlank) {
-          // send a standard http response header
+          // Enviar cabeçalhos:
           client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: application/json");
-          client.println("Access-Control-Allow-Origin: *");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Content-Type: application/json"); // O server deve retornar um JSON
+          client.println("Access-Control-Allow-Origin: *"); // Evita erros de CORS
+          client.println("Connection: close"); 
+
 
           client.println();
           client.print("{\"number\":");
@@ -97,17 +77,15 @@ void loop() {
           break;
         }
         if (c == '\n') {
-          // you're starting a new line
           currentLineIsBlank = true;
         } else if (c != '\r') {
-          // you've gotten a character on the current line
+
           currentLineIsBlank = false;
         }
       }
     }
-    // give the web browser time to receive the data
     delay(1);
-    // close the connection:
+    // Encerra a conexão 
     client.stop();
     Serial.println("client disconnected");
   }
